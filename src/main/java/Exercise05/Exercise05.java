a5ee6aefb0c0f934f192e01811bbf5786cbc068c
itsbot <patrickiannini@knights.ucf.edu>
2021-09-09 21:51:22 -0400

    Completed Exercise

--- src/main/java/Exercise05/Exercise05.java
+++ src/main/java/Exercise05/Exercise05.java
@@ -3,6 +3,25 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise05;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Exercise05 {
+    public static void main(String[] args) {
+        System.out.println("What is the first number?");
+        Scanner sc = new Scanner(System.in);
+        int int1 = sc.nextInt();
+
+        System.out.println("What is the second number?");
+        Scanner sc1 = new Scanner(System.in);
+        int int2 = sc1.nextInt();
+
+        int add = int1+int2;
+        int sub = int1-int2;
+        int mul = int1*int2;
+        double div = (double)int1/int2;
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println(int1+" + "+int2+" = "+add+"\n"+int1+" - "+int2+" = "+sub+"\n"+int1+" * "+int2+" = "+mul+"\n"+int1+" / "+int2+" = "+df.format(div));
+    }
 }
